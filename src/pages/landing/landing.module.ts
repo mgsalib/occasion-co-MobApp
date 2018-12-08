c67ab840eb5f87220f66f01741962b03162379c9
MGeorge_UI <mgeorge@three2one.net>
2018-12-08 15:03:15 +0200

    landing page

---
+++ src/pages/landing/landing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { LandingPage } from './landing';
+import { TranslateModule } from '@ngx-translate/core';
+import { ComponentsModule } from '../../components/components.module';
+
+@NgModule({
+  declarations: [
+    LandingPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LandingPage),
+    TranslateModule.forChild(),
+    ComponentsModule
+  ],
+})
+export class LandingPageModule {}
